10260bd7a8db46cf705a53cb35dd8bebd360570e
神州光大 <shenzhouguangda@wifi.airdream>
2016-09-12 17:06:32 +0800

    初始化项目

---
+++ ceshiyixiaxia/ceshiyixiaxia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshiyixiaxia
+//
+//  Created by 神州光大 on 16/9/12.
+//  Copyright © 2016年 神州光大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshiyixiaxia/ceshiyixiaxia/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  ceshiyixiaxia
+//
+//  Created by 神州光大 on 16/9/12.
+//  Copyright © 2016年 神州光大. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"测试一下一下");
+ }
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
